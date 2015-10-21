bace7ebef0b8496943f994fa783c27487f636688
evdevgit <stratfor@evereti.com>
2015-10-21 20:28:44 +0200

    Log actions

--- main.js
+++ main.js
@@ -9,14 +9,20 @@ var searchTerms = [
   {q: 'Cool', domain: '.co.uk'}
 ];
 
-for (var i = 0; i < searchTerms.length; i++) {
+var webBot = function(searchTerm, domain) {
   var driver = new webdriver.Builder()
       .forBrowser('firefox')
-      .setProxy(proxy.manual({http: '117.240.187.35:3128'}))
+      //.setProxy(proxy.manual({http: '117.240.187.35:3128'}))
       .build();
-  driver.get('http://www.google'+searchTerms[i].domain);
-  driver.findElement(By.name('q')).sendKeys(searchTerms[i].q);
+  driver.get('http://www.google'+domain);
+  console.log('Loading: google'+domain);
+  driver.findElement(By.name('q')).sendKeys(searchTerm);
   driver.findElement(By.name('btnG')).click();
-  driver.wait(until.titleIs(searchTerms[i].q+' - Google Search'), 1000);
+  console.log('Searching for: '+searchTerm);
+  driver.wait(until.titleIs(searchTerm+' - Google Search'), 1000);
   driver.quit();
+}
+
+for (var i = 0; i < searchTerms.length; i++) {
+  webBot(searchTerms[i].q, searchTerms[i].domain);
 }
